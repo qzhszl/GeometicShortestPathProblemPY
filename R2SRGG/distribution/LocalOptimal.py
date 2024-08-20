983077a283a8b5592dc2a7ebe32979021fd8d445
qzh <867483679@qq.com>
2024-08-20 21:32:54 +0200

    GOOD NIGHT 820 2132

--- R2SRGG/distribution/LocalOptimal.py
+++ R2SRGG/distribution/LocalOptimal.py
@@ -7,15 +7,69 @@ This script investigates the local optimum of the average distance from the shor
 We have already seen that with the increase of the average degree, the average distance goes up, then down, then up agian.
 We also want to study what will happen if we change the beta, i.e. the parameter that determines clustering coefficient
 """
+import itertools
+
 import numpy as np
 import networkx as nx
+import random
+import math
 
-def avedistance_inSRGG(network_size_index, average_degree_index, beta_index, simu_time):
-    Nvec = [10, 20, 50, 100, 1000, 10000]
-    kvec = list(range(2, 16)) + [20, 25, 30, 35, 40, 50, 60, 70, 80]
+from R2SRGG.R2SRGG import R2SRGG
+from SphericalSoftRandomGeomtricGraph import RandomGenerator
+
+def avedistance_insmallSRGG(N,ED,beta,rg):
     simu_times = 100
+    for simu_index in range(simu_times):
+        G, Coorx, Coory = R2SRGG(N, ED, beta, rg)
+        real_avg = 2 * nx.number_of_edges(G) / nx.number_of_nodes(G)
+        print("real ED:", real_avg)
+        print("clu:", nx.average_clustering(G))
+        components = list(nx.connected_components(G))
+        largest_component = max(components, key=len)
+        print("LCC", len(largest_component))
 
-    for simu
+        # pick up all the node pairs in the LCC
+        nodes = list(largest_component)
+        unique_pairs = set(tuple(sorted(pair)) for pair in itertools.combinations(nodes, 2))
+        count = 0
+
+
+
+
+def avedistance_inlargeSRGG(N,ED,beta,rg):
+    pass
+
+
+def avedistance_inSRGG(network_size_index, average_degree_index, beta_index, ExternalSimutime):
+    Nvec = [10, 20, 50, 100, 200, 500, 1000, 10000]
+    kvec = list(range(2, 16)) + [20, 25, 30, 35, 40, 50, 60, 70, 80, 100]
+    betavec = [2.1, 4, 8, 16, 32, 64, 128]
+
+    random.seed(ExternalSimutime)
+    rg = RandomGenerator(-12)
+    rseed = random.randint(0, 100)
+    print(rseed)
+    for i in range(rseed):
+        rg.ran1()
+
+    simu_times = 100
+    N = Nvec[network_size_index]
+    ED = kvec[average_degree_index]
+    beta = betavec[beta_index]
+
+
+
+    # for large network, we only generate one network and randomly selected 1,000 node pair.
+    # for small network, we
+    if N>100:
+        avedistance_inlargeSRGG(N,ED,beta,rg)
+    else:
+        # Random select nodepair_num nodes in the largest connected component
+        avedistance_insmallSRGG(N, ED, beta, rg)
+
+
+
+    for simu_index in range(simu_times):
         ExpectedDegreeVec = np.zeros(simu_times)
         LCCVec = np.zeros(simu_times)
         DistanceWithoutNorVec = []
@@ -161,8 +215,10 @@ def avedistance_inSRGG(network_size_index, average_degree_index, beta_index, sim
 
     # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print(math.comb(100,2))
     network_size_index = 0
     average_degree_index = 0
     beta_index = 0
-    simu_time = 0
-    avedistance_inSRGG(network_size_index, average_degree_index, beta_index, simu_time)
+    external_simu_time = 0
+    avedistance_inSRGG(network_size_index, average_degree_index, beta_index, external_simu_time)
+
