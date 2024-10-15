22f1d2d861858d806d8de5ca0e4f0d391c137671
qzh <867483679@qq.com>
2024-10-15 23:06:42 +0200

    GOOD NIGHT 1015 2306

--- R2SRGG/distribution/MeanfieldanalyticResult.py
+++ R2SRGG/distribution/MeanfieldanalyticResult.py
@@ -12,8 +12,8 @@ import random
 import math
 import sys
 
-from R2SRGG import R2SRGG, distR2, dist_to_geodesic_R2, loadSRGGandaddnode, R2SRGG_withgivennodepair
-# from R2SRGG.R2SRGG import R2SRGG, distR2, dist_to_geodesic_R2, loadSRGGandaddnode, R2SRGG_withgivennodepair
+# from R2SRGG import R2SRGG, distR2, dist_to_geodesic_R2, loadSRGGandaddnode, R2SRGG_withgivennodepair
+from R2SRGG.R2SRGG import R2SRGG, distR2, dist_to_geodesic_R2, loadSRGGandaddnode, R2SRGG_withgivennodepair
 from SphericalSoftRandomGeomtricGraph import RandomGenerator
 from main import all_shortest_path_node, find_k_connected_node_pairs, find_all_connected_node_pairs
 
@@ -117,49 +117,147 @@ def neighbour_distance_inlargeSRGG_clu_cc_givennodepair(N, ED, beta, cc, rg, Ext
 def neighbour_distance(network_size_index, average_degree_index, cc_index, Geodistance_index ,ExternalSimutime):
     # for control cc
     #----------------------------------------------------------------------------------------------------
-    # smallED_matrix = [[2.6, 2.6, 2.6, 2.6, 2.6, 0, 0],
-    #                   [4.3, 4, 4, 4, 4, 0],
-    #                   [5.6, 5.4, 5.2, 5.2, 5.2, 5.2, 5.2],
-    #                   [7.5, 6.7, 6.5, 6.5, 6.5, 6.5]]
-    # smallbeta_matrix = [[3.1, 4.5, 6, 300, 0, 0],
-    #                     [2.7, 3.5, 4.7, 7.6, 300, 0],
-    #                     [2.7, 3.4, 4.3, 5.7, 11, 300],
-    #                     [2.55, 3.2, 4, 5.5, 8.5, 300]]
-    #
-    # Nvec = [10, 100, 200, 500, 1000, 10000]
-    # kvec = list(range(2, 20)) + [20, 25, 30, 35, 40, 50, 60, 70, 80, 100]
-    # cc_vec = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-    # betavec = [2.55, 3.2, 3.99, 5.15, 7.99, 300]
-    #
-    # distance_list = [[0.49,0.5,0.5,0.5],[0.25, 0.25, 0.3, 0.3],[0.25, 0.25, 0.3, 0.3], [0.25, 0.25, 0.5, 0.5], [0.25, 0.25, 0.75, 0.75]]
-    # x_A = distance_list[Geodistance_index][0]
-    # y_A = distance_list[Geodistance_index][1]
-    # x_B = distance_list[Geodistance_index][2]
-    # y_B = distance_list[Geodistance_index][3]
-    # geodesic_distance_AB = round(x_B - x_A, 4)
-    #
-    # random.seed(ExternalSimutime)
-    # N = Nvec[network_size_index]
-    # target_ED = kvec[average_degree_index]
-    # if average_degree_index<4:
-    #     ED = smallED_matrix[average_degree_index][cc_index]
-    #     if ED == 0:
-    #         raise RuntimeError("Not exist")
-    #     beta = smallbeta_matrix[average_degree_index][cc_index]
+    smallED_matrix = [[2.6, 2.6, 2.6, 2.6, 2.6, 0, 0],
+                      [4.3, 4, 4, 4, 4, 0],
+                      [5.6, 5.4, 5.2, 5.2, 5.2, 5.2, 5.2],
+                      [7.5, 6.7, 6.5, 6.5, 6.5, 6.5]]
+    smallbeta_matrix = [[3.1, 4.5, 6, 300, 0, 0],
+                        [2.7, 3.5, 4.7, 7.6, 300, 0],
+                        [2.7, 3.4, 4.3, 5.7, 11, 300],
+                        [2.55, 3.2, 4, 5.5, 8.5, 300]]
+
+    Nvec = [10, 100, 200, 500, 1000, 10000]
+    kvec = list(range(2, 20)) + [20, 25, 30, 35, 40, 50, 60, 70, 80, 100]
+    cc_vec = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    betavec = [2.55, 3.2, 3.99, 5.15, 7.99, 300]
+
+    distance_list = [[0.49,0.5,0.5,0.5],[0.25, 0.25, 0.3, 0.3],[0.25, 0.25, 0.3, 0.3], [0.25, 0.25, 0.5, 0.5], [0.25, 0.25, 0.75, 0.75]]
+    x_A = distance_list[Geodistance_index][0]
+    y_A = distance_list[Geodistance_index][1]
+    x_B = distance_list[Geodistance_index][2]
+    y_B = distance_list[Geodistance_index][3]
+    geodesic_distance_AB = round(x_B - x_A, 4)
+
+    random.seed(ExternalSimutime)
+    N = Nvec[network_size_index]
+    target_ED = kvec[average_degree_index]
+    if average_degree_index<4:
+        ED = smallED_matrix[average_degree_index][cc_index]
+        if ED == 0:
+            raise RuntimeError("Not exist")
+        beta = smallbeta_matrix[average_degree_index][cc_index]
+    else:
+        ED = kvec[average_degree_index]
+        beta = betavec[cc_index]
+
+    C_G = cc_vec[cc_index]
+    print("input para:", (N, ED, beta,C_G,geodesic_distance_AB))
+
+
+    rg = RandomGenerator(-12)
+    rseed = random.randint(0, 100)
+    for i in range(rseed):
+        rg.ran1()
+
+    # for large network, we only generate one network and randomly selected 1,000 node pair.
+    # for small network, we generate 100 networks and selected all the node pair in the LCC
+    if N > 100:
+        neighbour_distance_inlargeSRGG_clu_cc_givennodepair(N, ED, beta, C_G,rg, ExternalSimutime,geodesic_distance_AB,x_A,y_A,x_B,y_B,target_ED)
     # else:
-    #     ED = kvec[average_degree_index]
-    #     beta = betavec[cc_index]
-    #
-    # C_G = cc_vec[cc_index]
-    # print("input para:", (N, ED, beta,C_G,geodesic_distance_AB))
+    #     # Random select nodepair_num nodes in the largest connected component
+    #     distance_insmallSRGG(N, ED, beta, rg, ExternalSimutime)
+    print("ok")
 
 
+def neighbour_distance_inlargeSRGG_clu_beta_givennodepair(N, ED, beta, rg, ExternalSimutime, geodesic_distance_AB,x_A,y_A,x_B,y_B):
+    """
+    :param N:
+    :param ED:
+    :param ExternalSimutime:
+    :return:
+    for each node pair, we record the ave,max,min of distance from the shortest path to the geodesic,
+    length of the geo distances.
+    The generated network, the selected node pair and all the deviation of both shortest path and baseline nodes will be recorded.
+    """
+    if N> ED:
+        deviation_vec = []  # deviation of all shortest path nodes for all node pairs
+        baseline_deviation_vec = []  # deviation of all shortest path nodes for all node pairs
+        # For each node pair:
+        ave_deviation = []
+        max_deviation = []
+        min_deviation = []
+        ave_baseline_deviation =[]
+        length_geodesic = []
+        hopcount_vec = []
+        SPnodenum_vec =[]
+
+        # load a network
+
+        # Randomly generate 10 networks
+        Network_generate_time = 10
+
+        for network in range(Network_generate_time):
+            # N = 100 # FOR TEST
+            G, Coorx, Coory = R2SRGG_withgivennodepair(N, ED, beta, rg, x_A, y_A, x_B, y_B)
+            real_avg = 2 * nx.number_of_edges(G) / nx.number_of_nodes(G)
+            print("real ED:", real_avg)
+            ave_clu = nx.average_clustering(G)
+            print("clu:", ave_clu)
+            components = list(nx.connected_components(G))
+            largest_component = max(components, key=len)
+            LCC_number = len(largest_component)
+            print("LCC", LCC_number)
+            nodei = N-2
+            nodej = N-1
+
+            # Find the common neighbours
+            common_neighbors = list(nx.common_neighbors(G, nodei, nodej))
+            SPnodenum_vec.append(len(common_neighbors))
+            if common_neighbors:
+                xSource = Coorx[nodei]
+                ySource = Coory[nodei]
+                xEnd = Coorx[nodej]
+                yEnd = Coory[nodej]
+                # length_geodesic.append(distR2(xSource, ySource, xEnd, yEnd)) # for test
+                # Compute deviation for the shortest path of each node pair
+                deviations_for_a_nodepair = []
+                for SPnode in common_neighbors:
+                    xMed = Coorx[SPnode]
+                    yMed = Coory[SPnode]
+                    dist, _ = dist_to_geodesic_R2(xMed, yMed, xSource, ySource, xEnd, yEnd)
+                    deviations_for_a_nodepair.append(dist)
+
+                deviation_vec = deviation_vec+deviations_for_a_nodepair
+
+                ave_deviation.append(np.mean(deviations_for_a_nodepair))
+                max_deviation.append(max(deviations_for_a_nodepair))
+                min_deviation.append(min(deviations_for_a_nodepair))
+
+        deviation_vec_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\neighbour_distance\\Givendistancedeviation_neighbour_nodes_N{Nn}ED{EDn}beta{betan}Simu{ST}Geodistance{Geodistance}.txt".format(
+            Nn = N,EDn=ED, betan=beta, ST=ExternalSimutime, Geodistance = geodesic_distance_AB)
+        np.savetxt(deviation_vec_name, deviation_vec)
+        # For each node pair:
+        ave_deviation_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\neighbour_distance\\Givendistanceave_neighbour_nodes_deviation_N{Nn}ED{EDn}beta{betan}Simu{ST}Geodistance{Geodistance}.txt".format(
+            Nn = N,EDn=ED, betan=beta, ST=ExternalSimutime, Geodistance = geodesic_distance_AB)
+        np.savetxt(ave_deviation_name, ave_deviation)
+        max_deviation_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\neighbour_distance\\Givendistancemax_neighbour_nodes_deviation_N{Nn}ED{EDn}beta{betan}Simu{ST}Geodistance{Geodistance}.txt".format(
+            Nn = N,EDn=ED, betan=beta, ST=ExternalSimutime, Geodistance = geodesic_distance_AB)
+        np.savetxt(max_deviation_name, max_deviation)
+        min_deviation_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\neighbour_distance\\Givendistancemin_neighbour_nodes_deviation_N{Nn}ED{EDn}beta{betan}Simu{ST}Geodistance{Geodistance}.txt".format(
+            Nn = N,EDn=ED, betan=beta, ST=ExternalSimutime, Geodistance = geodesic_distance_AB)
+        np.savetxt(min_deviation_name, min_deviation)
+        SPnodenum_vec_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\neighbour_distance\\Givendistanceneighbournodenum_N{Nn}ED{EDn}beta{betan}Simu{ST}Geodistance{Geodistance}.txt".format(
+            Nn = N,EDn=ED, betan=beta, ST=ExternalSimutime, Geodistance = geodesic_distance_AB)
+        np.savetxt(SPnodenum_vec_name, SPnodenum_vec, fmt="%i")
+
+
+def neighbour_distance_beta(network_size_index, average_degree_index, cc_index, Geodistance_index, ExternalSimutime):
     # for control beta
     # ----------------------------------------------------------------------------------------------------
     Nvec = [10, 100, 200, 500, 1000, 10000]
-    kvec = [2,5,10,20,100]
-    # cc_vec = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-    betavec = [2,4,8,16,32,64,128]
+    N = Nvec[network_size_index]
+    kvec = [2, 5, 10, 20, 100]
+    betavec = [2.2, 4, 8, 16, 32, 64, 128]
 
     distance_list = [[0.49, 0.5, 0.5, 0.5], [0.25, 0.25, 0.3, 0.3], [0.25, 0.25, 0.3, 0.3], [0.25, 0.25, 0.5, 0.5],
                      [0.25, 0.25, 0.75, 0.75]]
@@ -173,8 +271,7 @@ def neighbour_distance(network_size_index, average_degree_index, cc_index, Geodi
     ED = kvec[average_degree_index]
     beta = betavec[cc_index]
 
-    C_G = cc_vec[cc_index]
-    print("input para:", (N, ED, beta, C_G, geodesic_distance_AB))
+    print("input para:", (N, ED, beta, geodesic_distance_AB))
 
     rg = RandomGenerator(-12)
     rseed = random.randint(0, 100)
@@ -184,7 +281,8 @@ def neighbour_distance(network_size_index, average_degree_index, cc_index, Geodi
     # for large network, we only generate one network and randomly selected 1,000 node pair.
     # for small network, we generate 100 networks and selected all the node pair in the LCC
     if N > 100:
-        neighbour_distance_inlargeSRGG_clu_cc_givennodepair(N, ED, beta, C_G,rg, ExternalSimutime,geodesic_distance_AB,x_A,y_A,x_B,y_B,target_ED)
+        neighbour_distance_inlargeSRGG_clu_beta_givennodepair(N, ED, beta, rg, ExternalSimutime,
+                                                            geodesic_distance_AB, x_A, y_A, x_B, y_B)
     # else:
     #     # Random select nodepair_num nodes in the largest connected component
     #     distance_insmallSRGG(N, ED, beta, rg, ExternalSimutime)
@@ -239,11 +337,11 @@ if __name__ == '__main__':
     # """
     # Run code
     # """
-    ED = sys.argv[1]
-    cc_index = sys.argv[2]
-    Geodistance_index = sys.argv[3]
-    ExternalSimutime = sys.argv[4]
-    neighbour_distance(5, int(ED), int(cc_index), int(Geodistance_index), int(ExternalSimutime))
+    # ED = sys.argv[1]
+    # cc_index = sys.argv[2]
+    # Geodistance_index = sys.argv[3]
+    # ExternalSimutime = sys.argv[4]
+    # neighbour_distance(5, int(ED), int(cc_index), int(Geodistance_index), int(ExternalSimutime))
 
     # """
     # Run code locally
@@ -253,3 +351,13 @@ if __name__ == '__main__':
     # for ED in range(28):
     #     for ExternalSimutime in range(10):
     #         neighbour_distance(5, int(ED), int(cc_index), int(Geodistance_index), int(ExternalSimutime))
+
+    neighbour_distance_beta(5,0,0,0,0)
+    # """
+    # Run code beta
+    # """
+    # ED = sys.argv[1]
+    # cc_index = sys.argv[2]
+    # Geodistance_index = sys.argv[3]
+    # ExternalSimutime = sys.argv[4]
+    # neighbour_distance_beta(5, int(ED), int(cc_index), int(Geodistance_index), int(ExternalSimutime))
