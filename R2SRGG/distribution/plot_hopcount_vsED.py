04f52ca75c3e5271cf5fdfa0cd89040ceeb6ff09
zqiu1 <Z.Qiu-1@tudelft.nl>
2025-08-03 16:45:22 +0200

    2025.083

--- R2SRGG/distribution/plot_hopcount_vsED.py
+++ R2SRGG/distribution/plot_hopcount_vsED.py
@@ -16,7 +16,7 @@ def plot_hopcount_vs_ED(N, beta):
     kvec = [2.0,3.0, 4.0,5.0,6.0,8, 10,12, 16, 27, 44, 72, 118, 193, 316, 518, 848, 1389, 2276, 3727, 6105, 9999] # for all sp
     kvec = [10, 16, 27, 44, 72, 118, 193, 316, 518, 848, 1389, 2276, 3727, 6105,
             9999]  # for all sp
-    kvec = [10, 16, 27, 44, 72, 107, 120, 193, 316, 518, 848, 1389, 2276, 3727, 6105,
+    kvec = [11, 16, 27, 44, 73, 107, 120, 193, 316, 518, 848, 1389, 2276, 3727, 6105,
             9999]  # for one sp
 
     colors = ["#D08082", "#C89FBF", "#62ABC7", "#7A7DB1", '#6FB494']
@@ -24,37 +24,47 @@ def plot_hopcount_vs_ED(N, beta):
     fig, ax = plt.subplots(figsize=(6, 4.5))
     SP_hopcount_ave = []
     SP_hopcount_std = []
-    for ED in kvec:
-        hopcount_for_a_para_comb = np.array([])
-        if ED < 15:
+    # for ED in kvec:
+    #     hopcount_for_a_para_comb = np.array([])
+    #     if ED < 15:
+    #
+    #         for ExternalSimutime in range(50):
+    #             try:
+    #                 hopcount_vec_name = filefolder_name + "hopcount_sp_N{Nn}_ED{EDn}Beta{betan}Simu{ST}.txt".format(
+    #                     Nn=N, EDn=ED, betan=beta, ST=ExternalSimutime)
+    #                 hopcount_for_a_para_comb_10times = np.loadtxt(hopcount_vec_name)
+    #                 hopcount_for_a_para_comb = np.hstack(
+    #                     (hopcount_for_a_para_comb, hopcount_for_a_para_comb_10times))
+    #             except:
+    #                 print("datalost:",(ED,beta,ExternalSimutime))
+    #     else:
+    #         for ExternalSimutime in range(20):
+    #             try:
+    #                 hopcount_vec_name = filefolder_name + "hopcount_sp_N{Nn}_ED{EDn}Beta{betan}Simu{ST}.txt".format(
+    #                     Nn=N, EDn=ED, betan=beta, ST=ExternalSimutime)
+    #                 hopcount_for_a_para_comb_10times = np.loadtxt(hopcount_vec_name)
+    #                 hopcount_for_a_para_comb = np.hstack(
+    #                     (hopcount_for_a_para_comb, hopcount_for_a_para_comb_10times))
+    #             except:
+    #                 print("datalost:",(ED,beta,ExternalSimutime))
+    #     SP_hopcount_ave.append(np.mean(hopcount_for_a_para_comb))
+    #     SP_hopcount_std.append(np.std(hopcount_for_a_para_comb))
 
-            for ExternalSimutime in range(50):
-                try:
-                    hopcount_vec_name = filefolder_name + "hopcount_sp_N{Nn}_ED{EDn}Beta{betan}Simu{ST}.txt".format(
-                        Nn=N, EDn=ED, betan=beta, ST=ExternalSimutime)
-                    hopcount_for_a_para_comb_10times = np.loadtxt(hopcount_vec_name)
-                    hopcount_for_a_para_comb = np.hstack(
-                        (hopcount_for_a_para_comb, hopcount_for_a_para_comb_10times))
-                except:
-                    print("datalost:",(ED,beta,ExternalSimutime))
-        else:
-            for ExternalSimutime in range(20):
-                try:
-                    hopcount_vec_name = filefolder_name + "hopcount_sp_N{Nn}_ED{EDn}Beta{betan}Simu{ST}.txt".format(
-                        Nn=N, EDn=ED, betan=beta, ST=ExternalSimutime)
-                    hopcount_for_a_para_comb_10times = np.loadtxt(hopcount_vec_name)
-                    hopcount_for_a_para_comb = np.hstack(
-                        (hopcount_for_a_para_comb, hopcount_for_a_para_comb_10times))
-                except:
-                    print("datalost:",(ED,beta,ExternalSimutime))
-        SP_hopcount_ave.append(np.mean(hopcount_for_a_para_comb))
-        SP_hopcount_std.append(np.std(hopcount_for_a_para_comb))
+    kvec = [11, 16, 27, 44, 73, 107, 120, 193, 316, 518, 848, 1389]
+    SP_hopcount_ave = [np.float64(10.68658), np.float64(8.175736), np.float64(6.041096), np.float64(4.825936), np.float64(3.973012),
+     np.float64(3.50587), np.float64(3.381164), np.float64(2.959456), np.float64(2.63692), np.float64(2.38936),
+     np.float64(2.140096), np.float64(1.958244)]
+    SP_hopcount_std = [np.float64(3.2480486301162426), np.float64(2.3264283479840935), np.float64(1.5923162747343884),
+     np.float64(1.2057220757305558), np.float64(0.945885642060392), np.float64(0.8104107249413719),
+     np.float64(0.7713455808546515), np.float64(0.657793420508293), np.float64(0.5602186301793256),
+     np.float64(0.5534462850177965), np.float64(0.4775406901867107), np.float64(0.3507883071939542)]
 
     plt.errorbar(kvec,SP_hopcount_ave,SP_hopcount_std,linestyle="-", linewidth=3, elinewidth=1, capsize=5, marker='o', markersize=6)
 
     print(kvec)
     print(SP_hopcount_ave)
     print(SP_hopcount_std)
+
 
     # plt.xticks(np.arange(0, 50, 10))
     # plt.legend(fontsize=20)
@@ -63,6 +73,7 @@ def plot_hopcount_vs_ED(N, beta):
     # plt.xticks(fontsize=28)
     # plt.yticks(fontsize=28)
     plt.xscale('log')
+    plt.yscale('log')
     plt.xlabel(r'$E[D]$', fontsize=32)
     plt.ylabel(r'$E[h]$', fontsize=32)
     plt.xticks(fontsize=26)
