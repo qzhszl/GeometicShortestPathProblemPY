481e0c5735df862cbf3f8da2c8d4c4002b5447e1
qzh <867483679@qq.com>
2025-08-03 21:20:50 +0200

    GOOD NIGHT 20250803

--- R2SRGG/distribution/plot_hopcount_vsED.py
+++ R2SRGG/distribution/plot_hopcount_vsED.py
@@ -51,6 +51,15 @@ def plot_hopcount_vs_ED(N, beta):
     #     SP_hopcount_std.append(np.std(hopcount_for_a_para_comb))
 
     kvec = [11, 16, 27, 44, 73, 107, 120, 193, 316, 518, 848, 1389]
+    kvec = [0.886*i**(0.957) for i in kvec]
+    # x = [10, 16, 27, 44, 72, 118, 193, 316, 518, 848, 1389]
+    # a_realavg_vec = []
+    #
+    # kvec = [7.6544, 12.1272, 20.414358564143587,
+    #                 32.9682,
+    #                 53.2058, 85.6794, 137.1644, 218.4686, 345.3296, 541.029]
+
+
     SP_hopcount_ave = [np.float64(10.68658), np.float64(8.175736), np.float64(6.041096), np.float64(4.825936), np.float64(3.973012),
      np.float64(3.50587), np.float64(3.381164), np.float64(2.959456), np.float64(2.63692), np.float64(2.38936),
      np.float64(2.140096), np.float64(1.958244)]
@@ -65,9 +74,19 @@ def plot_hopcount_vs_ED(N, beta):
     print(SP_hopcount_ave)
     print(SP_hopcount_std)
 
+    k_vals  = np.linspace(min(kvec)-1,max(kvec)+100,50)
+    f1 = np.full_like(k_vals, np.log(N) / np.log(np.log(N)))
+    # 曲线 2: 1 + 1/p = 1 + n / k
+    f2 = 1 + N / k_vals
+    # 曲线 3: log(n) / log(k)
+    f3 = np.log(N) / np.log(k_vals)
+
+    plt.plot(k_vals, f1, label=r'$f_1(k) = \frac{\log n}{\log \log n}$', linestyle='--', color='orange')
+    plt.plot(k_vals, f2, label=r'$f_2(k) = 1 + \frac{n}{k}$', color='green')
+    plt.plot(k_vals, f3, label=r'$f_3(k) = \frac{\log n}{\log k}$', color='blue')
 
     # plt.xticks(np.arange(0, 50, 10))
-    # plt.legend(fontsize=20)
+    plt.legend(fontsize=20)
     # plt.xlabel(r'x', fontsize=35)
     # plt.ylabel(r'$f_{h}(x)$', fontsize=35)
     # plt.xticks(fontsize=28)
@@ -98,5 +117,109 @@ def plot_hopcount_vs_ED(N, beta):
     plt.close()
 
 
+def plot_hopcount_vs_ED_test(N, beta):
+    # load and test data
+    filefolder_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\largenetwork\\"
+    # filefolder_name = "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\OneSP\\"  # for ONE SP
+
+    kvec = [8,10, 13, 17, 22, 28, 36, 46, 58, 74, 94, 120, 155]
+
+
+    colors = ["#D08082", "#C89FBF", "#62ABC7", "#7A7DB1", '#6FB494']
+    count = 0
+    fig, ax = plt.subplots(figsize=(6, 4.5))
+    SP_hopcount_ave = []
+    SP_hopcount_std = []
+    for ED in kvec:
+        hopcount_for_a_para_comb = np.array([])
+
+        for ExternalSimutime in range(1):
+            try:
+                hopcount_vec_name = filefolder_name + "hopcount_sp_N{Nn}_ED{EDn}Beta{betan}Simu{ST}.txt".format(
+                    Nn=N, EDn=ED, betan=beta, ST=ExternalSimutime)
+                hopcount_for_a_para_comb_10times = np.loadtxt(hopcount_vec_name)
+                hopcount_for_a_para_comb = np.hstack(
+                    (hopcount_for_a_para_comb, hopcount_for_a_para_comb_10times))
+            except:
+                print("datalost:",(ED,beta,ExternalSimutime))
+
+        SP_hopcount_ave.append(np.mean(hopcount_for_a_para_comb))
+        SP_hopcount_std.append(np.std(hopcount_for_a_para_comb))
+
+
+    plt.errorbar(kvec,SP_hopcount_ave,SP_hopcount_std,linestyle="-", linewidth=3, elinewidth=1, capsize=5, marker='o', markersize=6)
+
+    print(kvec)
+    print(SP_hopcount_ave)
+    print(SP_hopcount_std)
+
+    k_vals  = np.linspace(min(kvec)-1,max(kvec)+100,50)
+    f1 = np.full_like(k_vals, np.log(N) / np.log(np.log(N)))
+    # 曲线 2: 1 + 1/p = 1 + n / k
+    f2 = 1 + N / k_vals
+    # 曲线 3: log(n) / log(k)
+    f3 = np.log(N) / np.log(k_vals)
+
+    plt.plot(k_vals, f1, label=r'$f_1(k) = \frac{\log n}{\log \log n}$', linestyle='--', color='orange')
+    plt.plot(k_vals, f2, label=r'$f_2(k) = 1 + \frac{n}{k}$', color='green')
+    plt.plot(k_vals, f3, label=r'$f_3(k) = \frac{\log n}{\log k}$', color='blue')
+
+    kvec = [11, 16, 27, 44, 73, 107, 120, 193, 316, 518, 848, 1389]
+    kvec = [0.886 * i ** (0.957) for i in kvec]
+    # x = [10, 16, 27, 44, 72, 118, 193, 316, 518, 848, 1389]
+    # a_realavg_vec = []
+    #
+    # kvec = [7.6544, 12.1272, 20.414358564143587,
+    #                 32.9682,
+    #                 53.2058, 85.6794, 137.1644, 218.4686, 345.3296, 541.029]
+
+    SP_hopcount_ave = [np.float64(10.68658), np.float64(8.175736), np.float64(6.041096), np.float64(4.825936),
+                       np.float64(3.973012),
+                       np.float64(3.50587), np.float64(3.381164), np.float64(2.959456), np.float64(2.63692),
+                       np.float64(2.38936),
+                       np.float64(2.140096), np.float64(1.958244)]
+    SP_hopcount_std = [np.float64(3.2480486301162426), np.float64(2.3264283479840935), np.float64(1.5923162747343884),
+                       np.float64(1.2057220757305558), np.float64(0.945885642060392), np.float64(0.8104107249413719),
+                       np.float64(0.7713455808546515), np.float64(0.657793420508293), np.float64(0.5602186301793256),
+                       np.float64(0.5534462850177965), np.float64(0.4775406901867107), np.float64(0.3507883071939542)]
+
+    plt.errorbar(kvec, SP_hopcount_ave, SP_hopcount_std, linestyle="-", linewidth=3, elinewidth=1, capsize=5,
+                 marker='o', markersize=6)
+
+    # plt.xticks(np.arange(0, 50, 10))
+    plt.legend(fontsize=20)
+    # plt.xlabel(r'x', fontsize=35)
+    # plt.ylabel(r'$f_{h}(x)$', fontsize=35)
+    # plt.xticks(fontsize=28)
+    # plt.yticks(fontsize=28)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel(r'$E[D]$', fontsize=32)
+    plt.ylabel(r'$E[h]$', fontsize=32)
+    plt.xticks(fontsize=26)
+    plt.yticks(fontsize=26)
+
+    text = r"$N = 10^4$" "\n" r"$\beta = 4$"
+    plt.text(
+        0.25, 0.65,  # 文本位置（轴坐标，0.5 表示图中央，1.05 表示轴上方）
+        text,
+        transform=ax.transAxes,  # 使用轴坐标
+        fontsize=20,  # 字体大小
+        ha='left',  # 水平居中对齐
+        va='bottom'  # 垂直对齐方式
+    )
+    plt.title("average hopcount vs expected degree")
+
+    picname =  "D:\\data\\geometric shortest path problem\\EuclideanSRGG\\max_min_ave_ran_deviation\\inpuavg_beta\\"+ "hopvsEDN{Nn}beta{betan}.pdf".format(
+        Nn=N, betan=beta)
+    # plt.savefig(picname, format='pdf', bbox_inches='tight', dpi=600)
+    plt.show()
+    # 清空图像，以免影响下一个图
+    plt.close()
+
+
+
 if __name__ == '__main__':
-    plot_hopcount_vs_ED(10000,4)
+    # plot_hopcount_vs_ED(10000,4)
+
+    plot_hopcount_vs_ED_test(1000, 128)
